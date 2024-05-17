26546ebf2ac5ac900d425cb9801500766f6047c0
reeseroo <reesewelty@utexas.edu>
2024-05-16 19:46:29 -0500

    Requirements

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #current power in summary
 
 
+import RPi.GPIO as GPIO
+import time
+
+bluePin = 14
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(bluePin, GPIO.OUT)
+
+GPIO.output(bluePin, GPIO.HIGH)
+
+try:
+        while 1:
+                GPIO.output(bluePin, GPIO.LOW)
+                time.sleep(0.75)
+                GPIO.output(bluePin, GPIO.HIGH)
+                time.sleep(0.75)
+except KeyboardInterrupt:
+        GPIO.cleanup()
